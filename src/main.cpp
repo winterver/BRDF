e33cdcaa51ad6e165a98fa1ae8a0fc5d96d5a68b
winterver <295107659@qq.com>
2024-07-17 08:40:58 +0800

    make DebugOutputCallback work

--- src/main.cpp
+++ src/main.cpp
@@ -368,6 +368,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
     constexpr int width = 1600, height = 900;
     GLFWwindow* window = glfwCreateWindow(width, height, "BRDF", nullptr, nullptr);
