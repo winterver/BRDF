724c36066b6eb0a262111699e19298204b6c67ed
winterver <295107659@qq.com>
2024-07-17 20:25:29 +0800

    various tests

--- src/main.cpp
+++ src/main.cpp
@@ -427,6 +427,101 @@ void APIENTRY DebugOutputCallback(GLenum source, GLenum type, GLuint id, GLenum 
     printf("Message : %s\n", message);
 }
 
+unsigned int sphereVAO = 0;
+GLsizei indexCount;
+void renderSphere()
+{
+    if (sphereVAO == 0)
+    {
+        glGenVertexArrays(1, &sphereVAO);
+
+        unsigned int vbo, ebo;
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec2> uv;
+        std::vector<glm::vec3> normals;
+        std::vector<unsigned int> indices;
+
+        const unsigned int X_SEGMENTS = 64;
+        const unsigned int Y_SEGMENTS = 64;
+        const float PI = 3.14159265359f;
+        for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+        {
+            for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+            {
+                float xSegment = (float)x / (float)X_SEGMENTS;
+                float ySegment = (float)y / (float)Y_SEGMENTS;
+                float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+                float yPos = std::cos(ySegment * PI);
+                float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+                positions.push_back(glm::vec3(xPos, yPos, zPos));
+                uv.push_back(glm::vec2(xSegment, ySegment));
+                normals.push_back(glm::vec3(xPos, yPos, zPos));
+            }
+        }
+
+        bool oddRow = false;
+        for (unsigned int y = 0; y < Y_SEGMENTS; ++y)
+        {
+            if (!oddRow) // even rows: y == 0, y == 2; and so on
+            {
+                for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+                {
+                    indices.push_back(y * (X_SEGMENTS + 1) + x);
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                }
+            }
+            else
+            {
+                for (int x = X_SEGMENTS; x >= 0; --x)
+                {
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                    indices.push_back(y * (X_SEGMENTS + 1) + x);
+                }
+            }
+            oddRow = !oddRow;
+        }
+        indexCount = static_cast<GLsizei>(indices.size());
+
+        std::vector<float> data;
+        for (unsigned int i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+        }
+        glBindVertexArray(sphereVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        unsigned int stride = (3 + 2 + 3) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
+    }
+
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+}
+
 int main()
 {
     glfwInit();
@@ -476,14 +571,21 @@ int main()
         loadTexture("models/MAC10_albedo.png", &albedoMap);
         loadTexture("models/MAC10_normal.png", &normalMap);
         makeTexture(255, 255, 255, 255, &metallicMap);
-        makeTexture(255, 255, 255, 255, &roughnessMap);
+        makeTexture(0, 0, 0, 255, &roughnessMap);
         */
         loadModel("models/Sphere.obj", &vbo, &ibo, &count);
+        /*
         loadTexture("models/rustediron2_basecolor.png", &albedoMap);
         loadTexture("models/rustediron2_normal.png", &normalMap);
         loadTexture("models/rustediron2_metallic.png", &metallicMap);
         loadTexture("models/rustediron2_roughness.png", &roughnessMap);
-        compileShaders(&program, pbr_vert, pbr_frag);
+        */
+        makeTexture(255, 255, 255, 255, &albedoMap);
+        makeTexture(128, 128, 255, 255, &normalMap);
+        makeTexture(255, 255, 255, 255, &metallicMap);
+        makeTexture(0, 0, 0, 255, &roughnessMap);
+        //compileShaders(&program, pbr_vert, pbr_frag);
+        compileShaders(&program, pbr_vert, pbr_frag_learnopengl);
         compileShaders(&skyboxprog, skybox_vert, skybox_frag);
         bakeHDR("models/dawn.hdr", &cubeMap, &irradianceMap, &prefilterMap);
         loadBRDFLUT("models/BRDF_LUT.dds", &brdflutMap);
@@ -580,8 +682,9 @@ int main()
         glActiveTexture(GL_TEXTURE6);
         glBindTexture(GL_TEXTURE_2D, brdflutMap);
 
-        glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, (void*)0);
+        //glBindVertexArray(vao);
+        //glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, (void*)0);
+        renderSphere();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
